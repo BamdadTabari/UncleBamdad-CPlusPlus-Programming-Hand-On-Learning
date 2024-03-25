75dff1db45ede214333d5ef02991a13ae36b0127
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-25 13:09:22 +0330

    2

---
+++ CPPLearn/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//In C++, cin.get(); 
+	// is a function that reads a single character from the standard input(usually the keyboard) 
+	// and stores it in the variable provided.
+	// If no variable is provided, it simply reads the character and discards it.
+	// This function is often used to pause the console window until the user presses a key, 
+	// which is useful for keeping the console window open after a program has finished executing.
+	cin.get(); // add this statement
+	cin.get(); // and maybe this, too
+	return 0;
+}
